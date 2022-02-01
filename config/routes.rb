e3a20f67b8a4274850d2f491072a0267d357b4cc
KatIsCoding <fabrigoru@gmail.com>
2022-02-01 09:32:16 -0600

    Route and page for showing posts of a specific user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'user'
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
