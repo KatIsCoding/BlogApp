19afae3349a128a6b3b5178ed6f66b35e4c32f94
KatIsCoding <fabrigoru@gmail.com>
2022-02-05 03:40:59 -0600

    Fix up references, this time referring to tables instead

--- db/migrate/20220204210655_add_post_ref_to_comment.rb
+++ db/migrate/20220204210655_add_post_ref_to_comment.rb
@@ -1,5 +1,5 @@
 class AddPostRefToComment < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :postid, null: false, foreign_key: true
+    add_reference :comments, :posts, foreign_key: true
   end
 end
