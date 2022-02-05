09bcb315115d2c3496ae9ec4d10ff6bda6b04209
KatIsCoding <fabrigoru@gmail.com>
2022-02-05 12:19:05 -0600

    Change type of big text containers

--- db/migrate/20220204211051_add_details_to_comments.rb
+++ db/migrate/20220204211051_add_details_to_comments.rb
@@ -1,6 +1,6 @@
 class AddDetailsToComments < ActiveRecord::Migration[7.0]
   def change
-    add_column :comments, :text, :string
+    add_column :comments, :text, :text
     add_column :comments, :updatedat, :datetime
     add_column :comments, :createdat, :datetime
   end
--- db/migrate/20220204211419_add_details_to_posts.rb
+++ db/migrate/20220204211419_add_details_to_posts.rb
@@ -1,7 +1,7 @@
 class AddDetailsToPosts < ActiveRecord::Migration[7.0]
   def change
     add_column :posts, :title, :string
-    add_column :posts, :text, :string
+    add_column :posts, :text, :text
     add_column :posts, :createdat, :datetime
     add_column :posts, :updatedat, :datetime
     add_column :posts, :commentscounter, :integer
