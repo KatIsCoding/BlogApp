d89ab9fdb9026f72aa1889323ee4c1f1dd2d833b
KatIsCoding <fabrigoru@gmail.com>
2022-02-11 13:15:14 -0600

    Add methos for creating comments, posts and likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,18 @@ class PostsController < ApplicationController
     @user = User.find(@user_id)
     @post = Post.find(@post_id)
   end
+
+  def create
+    Post.create(author: UsersController.current_user, title: params[:title], text: params[:text])
+  end
+
+  def create_comment
+    current_post = Post.find(params[:id])
+    current_post.comments.create(author: UsersController.current_user, text: params[:text])
+  end
+
+  def add_like
+    current_post = Post.find(params[:id])
+    Like.create(author: UsersController.current_user, post: current_post)
+  end
 end
