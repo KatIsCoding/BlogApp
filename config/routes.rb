8f52feadb930d1e9391edd56f77a8d1130851e32
KatIsCoding <fabrigoru@gmail.com>
2022-02-11 20:12:07 -0600

    Update routes to new methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :comment]
   end
-  post '/users/:user_id/posts/:id', to: 'posts#create_comment'
-  post '/like/:id', to: 'posts#add_like'
+  post '/users/:user_id/posts/:id', to: 'comments#create'
+  post '/like/:id', to: 'likes#create'
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
