30072748197e5b18a3fbbf0a97a7c520deb4616b
Nicolae Pop <nicupop729@gmail.com>
2022-02-28 18:22:33 -0300

    Fix code to pass linters checks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   # TODO: These are disabled fro now because there are some other implementations to do before being able o enable them
-  #validates :name, presence: true
-  #validates :postscounter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  # validates :name, presence: true
+  # validates :postscounter, presence: true, numericality: { greater_than_or_equal_to: 0 }
   # A method that returns the last 3 posts of a user using associations
 
   def last_posts
