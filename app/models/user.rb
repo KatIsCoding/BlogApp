c63fe82e6a91d4b12f8e56f74fc31bfee6b5bda7
KatIsCoding <fabrigoru@gmail.com>
2022-03-01 08:14:14 -0600

    Re enable validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,7 @@ class User < ActiveRecord::Base
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
-  # TODO: These are disabled fro now because there are some other implementations to do before being able o enable them
-  # validates :name, presence: true
-  # validates :postscounter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
   # A method that returns the last 3 posts of a user using associations
 
   def last_posts
